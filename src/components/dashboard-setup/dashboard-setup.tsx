12cd309a6c8d96da54def9194101528363353d0c
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-01 12:03:55 +0900

    update

--- src/components/dashboard-setup/dashboard-setup.tsx
+++ src/components/dashboard-setup/dashboard-setup.tsx
@@ -1,7 +1,18 @@
 'use client';
 import { AuthUser } from '@supabase/supabase-js';
-import React from 'react';
+import React, { useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import { subscriptions } from '../../../migrations/schema';
+import { v4 } from 'uuid';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import * as z from 'zod';
 
 type DashboardSetupProps = {
   user?: AuthUser;
