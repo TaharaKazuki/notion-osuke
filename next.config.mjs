1894db3179973c4e258195a4508e18cc73364b94
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-07 16:40:57 +0900

    update

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    domains: ['https://ggkmuyqgbglgvcckzroc.supabase.co'],
+  },
 };
 
 export default nextConfig;
